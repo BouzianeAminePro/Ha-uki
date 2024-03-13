a71b6207c00ccd3e963d9ebe9f1d2d7355972a6c
Ismail <ismailsaghraoui@gmail.com>
2024-03-13 23:14:31 +0100

    set up jenkins and sonarqube

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "standalone",
     typescript: {
         // !! WARN !!
         // Dangerously allow production builds to successfully complete even if
